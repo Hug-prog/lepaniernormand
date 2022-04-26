d7a7cf64b69afe04509393fdf3ed499f5216236e
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-04-26 10:00:55 +0200

    api call

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import { getAllCategories } from "../api/Categorie/Category";
+
 export default function Home() {
+  useEffect(() => {
+    getAllCategories();
+  });
+
   return (
     <div>
       <h1 classname="text-black">test</h1>
