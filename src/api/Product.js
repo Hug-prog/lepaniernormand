898b278afada2b30531883c46f1316a44b03b69b
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-04-27 19:08:28 +0200

    add productCateg

--- src/api/Product.js
+++ src/api/Product.js
@@ -2,15 +2,15 @@ import { API } from ".";
 import { GET_PRODUCTS } from "../constants/products";
 
 export const getAllProducts = () => {
-  return (dispatch) => {
+  return dispatch => {
     API.get("/api/products")
-      .then((res) => dispatch({ type: GET_PRODUCTS, payload: res.data }))
-      .catch((err) => console.log(err));
+      .then(res => dispatch({ type: GET_PRODUCTS, payload: res.data }))
+      .catch(err => console.log(err));
   };
 };
 
-export const getProductById = (id) => {
+export const getProductById = id => {
   API.get("/api/products" + id)
-    .then((res) => console.log(res.data))
-    .catch((err) => console.log(err));
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
 };
--- src/components/Category/CategoryCard.js
+++ src/components/Category/CategoryCard.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function CategoryCard({ category }) {
   return (
     <div className="w-32 h-32 bg-slate-100 mt-4 m-auto rounded ">
-      <Link to={"/"}>
+      <Link to={"/categories"} state={category.id}>
         <div className="w-full h-2/3 m-auto bg-slate-800 rounded"></div>
         <h1 className=" text-center ">{category.libelle}</h1>
       </Link>
