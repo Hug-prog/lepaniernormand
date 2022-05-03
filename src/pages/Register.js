0a8da232c1718e95c759ce8096a3075d77d91f31
Dylan Aumond <dylanaumond@gmail.com>
2022-05-03 21:07:35 +0200

    register form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -8,18 +8,48 @@ export default function Register() {
     password: "",
     confirmPassword: "",
   };
+  const [formData, setFormData] = useState(initForm);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   return (
     <div>
       <p>Créer un compte</p>
       <form>
-        <input type="email" name="email" placeholder="Adresse e-mail" />
-        <input type="text" name="firstname" placeholder="Prénom" />
-        <input type="text" name="lastname" placeholder="Nom" />
-        <input type="password" name="password" placeholder="mot de passe" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Adresse e-mail"
+          value={formData.email}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="text"
+          name="firstname"
+          placeholder="Prénom"
+          value={formData.firstName}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="text"
+          name="lastname"
+          placeholder="Nom"
+          value={formData.lastname}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="mot de passe"
+          value={formData.password}
+          onChange={(e) => handleChange(e)}
+        />
         <input
           type="password"
           name="confirmPassword"
           placeholder="comfirmez mot de passe"
+          value={formData.confirmPassword}
+          onChange={(e) => handleChange(e)}
         />
         <input type="submit" />
       </form>
