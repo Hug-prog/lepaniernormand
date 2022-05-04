f6e3d9871647c4a31bcfa999edc2afde232cdc5c
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-05-04 15:47:23 +0200

    form product

--- src/components/Product/FormProduct.js
+++ src/components/Product/FormProduct.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCategories } from "../../api/Category";
 import { addProduct } from "../../api/Product";
+import { getSellers } from "../../api/Sellers";
 
 export default function FormProduct() {
   const dispatch = useDispatch();
@@ -14,18 +16,27 @@ export default function FormProduct() {
     categoriesId: [],
     sellerId: "",
   };
+  const categories = useSelector(state => state.categoriesReducer);
+  const sellers = useSelector(state => state.sellersReducer);
+
+  console.log(sellers);
+
+  useEffect(() => {
+    dispatch(getCategories());
+    dispatch(getSellers());
+  }, [dispatch]);
 
   const [product, setProduct] = useState(initForm);
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
 
-  const handleFiles = (e) => {
+  const handleFiles = e => {
     setProduct({ ...product, images: e.target.files });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     for (const [key, value] of Object.entries(product)) {
       if (key === "images") {
@@ -46,13 +57,14 @@ export default function FormProduct() {
 
   return (
     <div>
-      <form onSubmit={(e) => handleSubmit(e)}>
+      <form onSubmit={e => handleSubmit(e)}>
         <label>libelle</label>
         <input
           type={"text"}
           name={"libelle"}
           value={product.libelle}
-          onChange={(e) => handleChange(e)}
+          onChange={e => handleChange(e)}
+          className="block"
         />
 
         <label>prix</label>
@@ -60,7 +72,8 @@ export default function FormProduct() {
           type={"text"}
           name={"price"}
           value={product.price}
-          onChange={(e) => handleChange(e)}
+          onChange={e => handleChange(e)}
+          className="block"
         />
 
         <label>stock</label>
@@ -68,35 +81,54 @@ export default function FormProduct() {
           type={"text"}
           name={"stock"}
           value={product.stock}
-          onChange={(e) => handleChange(e)}
+          onChange={e => handleChange(e)}
+          className="block"
         />
 
-        <label>description</label>
+        <label className="block">description</label>
         <input
           type={"text"}
           name={"description"}
           value={product.description}
-          onChange={(e) => handleChange(e)}
+          onChange={e => handleChange(e)}
         />
-        <label>sellerId</label>
-        <input
-          type={"text"}
-          name={"sellerId"}
-          value={product.sellerId}
-          onChange={(e) => handleChange(e)}
-        />
-        <label>categorieId</label>
-        <input
-          type={"text"}
-          name={"categorieId"}
-          value={product.categorieId}
-          onChange={(e) => handleChange(e)}
-        />
-        <label>images</label>
+        <label className="block">seller</label>
+        <select>
+          <option value="">seller</option>
+          {sellers
+            ? sellers.map((seller, i) => (
+                <option
+                  key={i}
+                  value={seller.id}
+                  onChange={e => handleChange(e)}
+                >
+                  {seller.libelle}
+                </option>
+              ))
+            : ""}
+        </select>
+
+        <label className="block">categories</label>
+        <select>
+          <option value="">categorie</option>
+          {categories
+            ? categories.map((category, i) => (
+                <option
+                  key={i}
+                  value={category.id}
+                  onChange={e => handleChange(e)}
+                >
+                  {category.libelle}
+                </option>
+              ))
+            : ""}
+        </select>
+
+        <label className="block">images</label>
         <input
           type={"file"}
           name="images"
-          onInput={(e) => handleFiles(e)}
+          onInput={e => handleFiles(e)}
           multiple
         />
         <button type="submit">ajouter</button>
