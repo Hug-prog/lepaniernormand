ccb9eb41057926e187aa139bc846c7ef8b311cac
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-05-11 18:28:23 +0200

    product style respons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="h-40 w-full static bottom-0 bg-black text-white">
+    <div className="h-20 w-full static bottom-0 bg-black text-white">
       Footer
     </div>
   );
