7b50729ad623f012dc9ba16ecbcaf2f1a3838e7d
Dylan Aumond <dylanaumond@gmail.com>
2022-05-12 12:50:13 +0200

    fix card categ

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -2,21 +2,24 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 import { getProductByCategoryId } from "../api/Category";
+import { getCurrentUser } from "../api/User";
 import ProductCard from "../components/Product/ProductCard";
 
 export default function Categories() {
   const dispatch = useDispatch();
   const location = useLocation();
-  const productsCateg = useSelector(state => state.productsReducer);
+  const productsCateg = useSelector((state) => state.productsReducer);
+  const user = useSelector((state) => state.userReducer);
   useEffect(() => {
     dispatch(getProductByCategoryId(location.state));
+    dispatch(getCurrentUser());
   }, [dispatch, location]);
 
   return (
     <div className="sm:flex sm:flex-wrap">
       {productsCateg
         ? productsCateg.map((product, i) => (
-            <ProductCard product={product} key={i} />
+            <ProductCard product={product} user={user} key={i} />
           ))
         : ""}
     </div>
